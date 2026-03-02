cbd33db4f59c2b677725e9b7e9b5984f461dd2b7
asabya <asabya@gmail.com>
2026-03-02 18:01:16 +0530

    feat: add Exchange abstract interface

---
+++ ipfs_lite/exchange/interface.py
@@ -0,0 +1,19 @@
+# ipfs_lite/exchange/interface.py
+from abc import ABC, abstractmethod
+from typing import List, Optional
+
+from multiformats import CID
+
+from ipfs_lite.block import Block
+
+
+class Exchange(ABC):
+    """Abstract interface for block exchange over the network."""
+
+    @abstractmethod
+    async def get_block(self, cid: CID, peers: List) -> Optional[Block]:
+        """Fetch a block from the network. Returns None if not found."""
+
+    @abstractmethod
+    async def has_block(self, block: Block) -> None:
+        """Announce to connected peers that we have a block."""
